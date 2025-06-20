bd178f1a29d3665dbf36ccf38ea8fb01b9f8d6bf
shrutirathod12 <rathod12shruti@gmail.com>
2025-06-20 20:32:13 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  )
+}
